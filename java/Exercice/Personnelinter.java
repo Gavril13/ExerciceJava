483ab43a5d62757cbcf8342d31a685002579fe7f
GavB0ss <gavrilou.bosso@gmail.com>
2023-03-06 14:16:55 +0100

    Test interface

---
+++ java/Exercice/Personnelinter.java
@@ -0,0 +1,11 @@
+public class Personnelinter implements interface2 {
+
+    @Override
+    public void Personnel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Personnel'");
+    }
+    
+}
+
+//fonctionnement de l'interface
